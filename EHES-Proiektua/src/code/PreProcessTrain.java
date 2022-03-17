e9643a801827d5043d2ec67d59761c86ad5bc168
Jon <jblanco044@ikasle.ehu.eus>
2022-03-17 10:13:08 +0100

    preprocesstrain con supervised AttributeSelection

--- EHES-Proiektua/src/code/PreProcessTrain.java
+++ EHES-Proiektua/src/code/PreProcessTrain.java
@@ -1,10 +1,15 @@
 package code;
 
+
+import weka.attributeSelection.BestFirst;
+import weka.attributeSelection.CfsSubsetEval;
 import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
 
 public class PreProcessTrain {
 	//hola
-	public static Instances preProcess(String dataPath) {
+	public static Instances preProcess(String dataPath) throws Exception {
 		PreProcessTrain ppt = new PreProcessTrain();
 		
 		Instances dataTrain = ppt.raw2arff(dataPath);
@@ -22,8 +27,16 @@ public class PreProcessTrain {
 		return null;
 	}
 	
-	public Instances fss(Instances data) {
-		return null;
+	public Instances fss(Instances data) throws Exception {
+		AttributeSelection aSelection = new AttributeSelection();
+		CfsSubsetEval cfsSubsetEval = new CfsSubsetEval();
+		BestFirst bFirst = new BestFirst();
+		aSelection.setEvaluator(cfsSubsetEval);
+		aSelection.setSearch(bFirst);
+		aSelection.setInputFormat(data);
+		Instances selectedData = Filter.useFilter(data, aSelection);
+		return (selectedData);
+		
 	}
 	
 }
