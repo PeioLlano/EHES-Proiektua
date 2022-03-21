44786d04331869d380a0d3d38432adf8fa22c309
peiol <peiollano@gmail.com>
2022-03-21 10:46:46 +0100

    Iragarpenak

--- EHES-Proiektua/src/code/IragarpenSortzailea.java
+++ EHES-Proiektua/src/code/IragarpenSortzailea.java
@@ -10,17 +10,19 @@ import weka.core.Instances;
 
 public class IragarpenSortzailea {
 
-	public static void eginIragarpenak(Instances dataTest, MultilayerPerceptron mp) throws Exception {
+	public static void eginIragarpenak(Instances dataTest, MultilayerPerceptron mp, String pathToSave) throws Exception {
 		
 		//ECLIPSE-tik FILE-era pasatzeaz arduratuko den 'stremer'-a.
-		FileOutputStream fos = new FileOutputStream(" -------------Non doan jarri behar da-------------");
+		FileOutputStream fos = new FileOutputStream(pathToSave);
 		PrintStream ps = new PrintStream(fos);
 
 		//Iragarpenak egiteaz arduratuko den ebaluatzailea.
 		Evaluation ev = new Evaluation(dataTest);
 		
-		ps.println("=======================PREDICTIONS=============================");
-		ps.println(" K  REAL PREDICTED (* -> erroreak)");
+		ps.println("-----------------------------------------------------------------");
+		ps.println("-                           PREDICTIONS                         -");
+		ps.println("-----------------------------------------------------------------");
+		ps.println("|  INSTANTZIA ZENB   |   REAL    |   PREDICTED   |   ERROR (*)   |");
 		
 		//Uneko intantzea zenbatzeaz arduratuko den parametroa.
         int k = 0;
@@ -35,7 +37,7 @@ public class IragarpenSortzailea {
             double prediction = ev.evaluateModelOnce(mp, instance);
             
             //Iragarpena eta erreala fitxategian gorde eta 'error' parametroa eguneratu
-            ps.printf("%2d.%4.0f%4.0f", ++k , actual, prediction);
+            ps.print("|          " + ++k +"          |     "+  (int)actual + "     |       " +  (int)prediction + "       |       " + (prediction != actual? "*        |": "         |"));
             ps.println(prediction != actual? " *": "");
             if(prediction != actual) error++;
         }
@@ -43,6 +45,8 @@ public class IragarpenSortzailea {
 		//Iragarpenen datuak fitxategian gorde.
 		ps.println("EGINDAKO PREDIZIO KOP= " + dataTest.numInstances());
 		ps.println("PREDIZIO ERRATU KOP= " + error);
+		ps.println("ERRORE PORTZENTAIA= " + error*100.0/dataTest.numInstances());
+
 		
 		ps.close();
 	}
